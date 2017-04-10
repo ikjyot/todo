2378c288821fbe56492d8432ce283b58f149b576
Ikjyot <isg6@njit.edu>
2017-04-10 14:26:17 -0400

    Added deletetodoitem function

--- db.php
+++ db.php
@@ -1,10 +1,20 @@
 <?php
 
+function deleteTodoItem($user_id, $todo_id) {
+  global $db;
+  $query = 'DELETE FROM todos WHERE id = :todo_id and user_id = :user_id';
+  $statement = $db->prepare($query);
+  $statement->bindValue(':user_id', $user_id);
+  $statement->bindValue(':todo_text', $todo_id);
+  $statement->execute();
+  $statement->closeCursor();
+}
+
 function addTodoItem($user_id, $todo_text) {
   global $db;
-  $query = "INSERT INTO todos(user_id, todo_item) values (:userid, :todo_text)";
+  $query = "INSERT INTO todos(user_id, todo_item) values (:user_id, :todo_text)";
   $statement = $db->prepare($query);
-  $statement->bindValue(':userid', $user_id);
+  $statement->bindValue(':user_id', $user_id);
   $statement->bindValue(':todo_text', $todo_text);
   $statement->execute();
   $statement->closeCursor();

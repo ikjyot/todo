d53a39e03523254809f61c5e2279a73ada66d851
Ikjyot <isg6@njit.edu>
2017-04-17 13:48:16 -0400

    Removed bug from details.php

--- details.php
+++ details.php
@@ -5,5 +5,31 @@
  * Date: 4/10/2017
  * Time: 2:37 PM
  */
-echo "Welcome";
-?>
+require('db_connection.php');
+require('db.php');
+
+
+if (isset($_GET['todo']) && isset($_COOKIE['my_id'])){
+  $result = getTodoItem($_COOKIE['my_id'], $_GET['todo']);
+  ?>
+
+<table>
+  <tr>
+    <td>Title:</td>
+    <td><?php echo $result['todo_item']?></td>
+  </tr>
+  <tr>
+    <td>Description:</td>
+    <td><?php echo $result['description']?></td>
+  </tr>
+  <tr>
+    <td>Start</td>
+    <td><?php echo $result['start_date']?></td>
+  </tr>
+  <tr>
+    <td>End</td>
+    <td><?php echo $result['end_date']?></td>
+  </tr>
+</table>
+
+<?php } ?>

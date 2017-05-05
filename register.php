c991299c4b24c0a9f07e8fa94568d9eb97a94170
Ikjyot <isg6@njit.edu>
2017-05-05 01:53:41 -0400

    Added all required fields in registration form

--- register.php
+++ register.php
@@ -1,14 +1,17 @@
-<?php?>
-
+<?php ?>
 <html>
 <head>
   <title>Register</title>
 </head>
-
 <body>
   <h1>This is a register page</h1>
   <form method="post" action="index.php">
-    <strong>Username: </strong> <input type="text" name="reg_username" value="" /> <br> <br>
+    <strong>First Name: </strong> <input type="text" name="reg_first_name" value="" /> <br> <br>
+    <strong>Last Name: </strong> <input type="text" name="reg_last_name" value="" /> <br> <br>
+    <strong>Phone: </strong> <input type="text" name="reg_phone" value="" /> <br> <br>
+    <strong>Birthday: </strong> <input type="text" name="reg_birthday" placeholder="yyyy-mm-dd" value="" /> <br> <br>
+    <strong>Gender: </strong> Male: <input type="radio" name="reg_gender" value="M" /> Female <input type="radio" name="reg_gender" value="F" /> <br> <br>
+    <strong>Email: </strong> <input type="text" name="reg_email" value="" /> <br> <br>
     <strong>Password: </strong> <input type="password" name="reg_password" value="" /> <br> <br>
     <input type="hidden" name="action" value="registrar" /> <br> <br>
     <input type="submit" value="register" />

3af37710ab0003c35f79c3e3c6af46de5b8c4a56
Ikjyot <isg6@njit.edu>
2017-05-05 09:47:43 -0400

    Removed Experimental logout button

--- login.php
+++ login.php
@@ -1,22 +1,24 @@
 <?php
 session_start();
-?>
+include "header.inc.php";
+if (!isset($_SESSION['name'], $_SESSION['user_id'], $_SESSION['isLogged'])) {
+  ?>
 
-<html>
-<body>
   <h1>Login</h1>
-  <form method = "post" action="index.php">
-  <strong>Email:</strong><input type="email" name="reg_email" value=""/><br><br>
-  <strong>Password:</strong><input type="password" name="reg_password" value=""/><br><br>
-  <input type ="hidden" name="action" value="test_user"><br>
-  <input type="submit" value="Login"/>
+  <form method="post" action="index.php">
+    <strong>Email:</strong><input type="email" name="reg_email" value=""/><br><br>
+    <strong>Password:</strong><input type="password" name="reg_password" value=""/><br><br>
+    <input type="hidden" name="action" value="test_user"><br>
+    <input type="submit" value="Login"/>
   </form>
 
   <form action="register.php">
     <input type="submit" value="Register">
   </form>
-  <form action="logout.php">
-    <input type="submit" value="Logout">
-  </form>
-</body>
-</html>
+
+<?php
+} else {
+  echo 'Already Logged In! <a href="index.php">Go back?</a>';
+}
+include "footer.inc.php";
+?>

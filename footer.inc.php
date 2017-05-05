625e48f752aa4e058471fc330dfbb683e0bb939c
Ikjyot <isg6@njit.edu>
2017-05-05 00:59:17 -0400

    Added header and footer files

---
+++ footer.inc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: isg6
+ * Date: 5/4/2017
+ * Time: 10:30 PM
+ */
+?>
+</main>
+<footer>
+
+</footer>
+<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
---
+++ header.inc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: isg6
+ * Date: 5/4/2017
+ * Time: 9:29 PM
+ */
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Ik's Todo</title>
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link type="text/css" rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+</head>
+<body>
+<header>
+
+</header>
+<main>

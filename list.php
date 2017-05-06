441744e993ca33839a434a305f3ee0f3ab7e4d25
Ikjyot <isg6@njit.edu>
2017-05-06 19:30:47 -0400

    Added some basic headings and section divider on list page

--- list.php
+++ list.php
@@ -8,52 +8,54 @@
 session_start();
 include 'header.inc.php';
 if (isset($_SESSION['name'], $_SESSION['user_id'], $_SESSION['isLogged'])) {
-echo "<h1>To Do List System</h1>";
-echo "Welcome, ".$_SESSION['name'].'<br />';
-echo "Below, you may find your to-do items <br /><br />";
+echo "<h5>Welcome, ".$_SESSION['name'].'</h5>';
+echo "<h5>Below, you may find your to-do items </h5>";
 ?>
+    <?php
+      global $result1, $result2;
+    ?>
+    <strong>ToDo Items</strong>
+    <?php
+      if (!empty($result1)) {
+        foreach ($result1 as $res1):?>
+          <ul id="todo" class="collapsible popout">
+            <li>
+              <div class="collapsible-header"><?php echo $res1['todo_title'] ?></div>
+              <div class="collapsible-body">I am collapsible</div>
+            </li>
+          </ul>
+          <form action="index.php" method="post">
+            <input type="hidden" name="item_id" value="<?php echo $res1['id'] ?>">
+            <input type="hidden" value="delete" name="action">
+            <button type="submit" class="waves-light waves-button-input hoverable btn">Delete</button>
+          </form>
+        <?php endforeach;
+      } else {
+        echo '<h6>You have no pending items</h6>';
+      } ?>
+  <!--The Section Divider goes <here-->
+  <div class="divider"></div>
+  <strong>Completed Items</strong>
 
-  <table>
-      <?php
-      global $result;
-      foreach($result as $res):?>
-    <tr>
-      <td><a href="details.php?todo=<?php echo $res['todo_item'] ?>"><?php echo $res['todo_item'] ?></a></td>
-      <td>
-        <form action="index.php" method="post">
-          <input type="hidden" name="item_id" value="<?php echo $res['id'] ?>">
-          <input type="hidden" value="delete" name="action">
-          <input type="submit" value="Delete">
-        </form>
-      </td>
-    </tr>
-      <?php endforeach; ?>
-  </table><br><br>
+  <?php
+  if (!empty($result2)) {
+    foreach ($result2 as $res2):?>
+      <ul id="todo" class="collapsible popout">
+        <li>
+          <div class="collapsible-header"><?php echo $res2['todo_title'] ?></div>
+          <div class="collapsible-body">I am collapsible</div>
+        </li>
+      </ul>
+      <form action="index.php" method="post">
+        <input type="hidden" name="item_id" value="<?php echo $res2['id'] ?>">
+        <input type="hidden" value="delete" name="action">
+        <button type="submit" class="waves-light waves-button-input hoverable btn">Delete</button>
+      </form>
+    <?php endforeach;
+  } else {
+    echo '<h6>You have no completed items</h6>';
+  } ?>
 
-  <!--<form method="post" action="index.php">
-    <input type="hidden" name="action" value="add">
-    <table>
-      <tr>
-        <td><label for="title">Title: </label></td>
-        <td><input type="text" name="title" id="title" required></td>
-      </tr>
-      <tr>
-        <td><label for="description">Description: </label></td>
-        <td><textarea id="description" name="description"></textarea></td>
-      </tr>
-      <tr>
-        <td><label for="start_date">Start</label></td>
-        <td><input type="date" id="start_date" name="start_date"></td>
-      </tr>
-      <tr>
-        <td><label for="end_date">End</label></td>
-        <td><input type='date' id="end_date" name="end_date"></td>
-      </tr>
-      <tr>
-        <td><input type="submit" value="Add"></td>
-      </tr>
-    </table>
-  </form>-->
 <?php
 } else {
   header("Location: index.php");

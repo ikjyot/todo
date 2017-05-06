0d125d512b5a79dfad391309dc0bcdd1218e067c
Ikjyot <isg6@njit.edu>
2017-05-06 16:48:12 -0400

    Register form refinement and logout functionality implemented

--- register.php
+++ register.php
@@ -3,53 +3,51 @@ include 'header.inc.php';
 if (!isset($_SESSION['name'], $_SESSION['user_id'], $_SESSION['isLogged'])) {
   ?>
   <!--Page Content---------------------------------------------------------->
-  <h2>Sign Up</h2>
-  <form method="POST" action="index.php">
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="reg_first_name" name="reg_first_name">
-      <label class="mdl-textfield__label" for="reg_first_name">First Name</label>
-    </div>
-    <br>
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="reg_last_name" name="reg_last_name">
-      <label class="mdl-textfield__label" for="reg_last_name">Lastname</label>
-    </div>
-    <br>
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="reg_phone" name="reg_phone">
-      <label class="mdl-textfield__label" for="reg_phone">Phone</label>
-    </div>
-    <br>
-    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-      <input class="mdl-textfield__input" type="text" id="reg_birthday" name="reg_birthday">
-      <label class="mdl-textfield__label" for="reg_birthday">Birthday</label>
-    </div>
-    <br>
-    <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="reg_genderM">
-      <input type="radio" id="reg_genderM" class="mdl-radio__button" name="reg_gender" value="M" checked>
-      <span class="mdl-radio__label">Male</span>
-    </label>
-    <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="reg_genderF">
-      <input type="radio" id="reg_genderF" class="mdl-radio__button" name="reg_gender" value="F">
-      <span class="mdl-radio__label">Female</span>
-    </label>
-    <br>
-      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-        <input class="mdl-textfield__input" autocomplete="off" type="email" id="reg_email" name="reg_email">
-        <label class="mdl-textfield__label" for="reg_email">Email</label>
+  <div class="register_section z-depth-4 hoverable">
+    <h2>Sign Up</h2>
+    <form method="POST" action="index.php">
+      <div class="input-field">
+        <input class="validate" type="text" id="reg_first_name" name="reg_first_name">
+        <label class="" for="reg_first_name">First Name</label>
       </div>
+      <div class="input-field">
+        <input class="validate" type="text" id="reg_last_name" name="reg_last_name">
+        <label for="reg_last_name">Last Name</label>
+      </div>
+      <div class="input-field">
+        <input class="validate" type="text" id="reg_phone" name="reg_phone">
+        <label for="reg_phone">Phone</label>
+      </div>
+
+      <input class="datepicker" id="reg_birthdate" placeholder="Birthday" type="date"  name="reg_birthday">
+
+      <p style="display: inline">
+        <input type="radio" id="reg_genderM" class="" name="reg_gender" value="M" checked>
+        <label for="reg_genderM">Male</label>
+      </p>
+      <p style="display: inline">
+        <input type="radio" id="reg_genderF" class="" name="reg_gender" value="M">
+        <label for="reg_genderF">Female</label>
+      </p>
       <br>
-      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-        <input class="mdl-textfield__input" type="password" id="reg_password" name="reg_password">
-        <label class="mdl-textfield__label" for="reg_password">Password</label>
+      <div class="input-field">
+        <input class="validate" type="email" id="reg_email" name="reg_email">
+        <label for="reg_email">Email</label>
       </div>
-    <br><br>
-    <input type="hidden" name="action" value="registrar">
-    <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--primary">
-      Sign Up
-    </button> &nbsp; &nbsp; Registered? <a href="login.php">Login</a>
-  </form>
-  <!--Page Content---------------------------------------------------------->
+
+      <div class="input-field">
+        <input class="validate" type="password" id="reg_password" name="reg_password">
+        <label for="reg_password">Password</label>
+      </div>
+      <input type="hidden" name="action" value="registrar">
+      <div class="signup_btn_div">
+        <button type="submit" class="waves-effect waves-light btn hoverable red">
+          Sign Up
+        </button> &nbsp; &nbsp; <span style="color: grey">Already Registered?</span> <a href="login.php"><span class="hoverable" style="color: #f44336; ">Login</span></a>
+      </div>
+    </form>
+  </div>
+    <!--Page Content---------------------------------------------------------->
 <?php
 } else {
   header("Location: index.php");

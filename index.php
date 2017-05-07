948b753944fe114e108da15f6f273d530dc5fafc
Ikjyot <isg6@njit.edu>
2017-05-07 04:21:22 -0400

    Delete function works now

--- index.php
+++ index.php
@@ -6,39 +6,28 @@ require('db.php');
 
 $action = filter_input(INPUT_POST, "action");
 
-if($action == NULL && !isset($_SESSION['name'], $_SESSION['user_id'], $_SESSION['isLogged']))
-{
-  $action = "show_login_page";
-}
+if (isset($_SESSION['name'], $_SESSION['user_id'], $_SESSION['isLogged'])) {
 
-elseif ($action == NULL && isset($_SESSION['name'], $_SESSION['user_id'], $_SESSION['isLogged'])) {
-  $result1 = getTodoItems($_SESSION['user_id'], 'pending');
-  $result2 = getTodoItems($_SESSION['user_id'], 'done');
-  include 'list.php';
-}
-
-if($action == "show_login_page")
-{
-  include('login.php');
-}
-
-else if($action == 'test_user')
-{
-  $email = filter_input(INPUT_POST, 'reg_email');
-  $password = filter_input(INPUT_POST, 'reg_password');
-  $valid_user = isUserValid($email,$password);
-  if($valid_user === true)
-  {
-    $result = getTodoItems($_SESSION['user_id']);
+  if ($action == '') {
+    $result1 = getTodoItems($_SESSION['user_id'], 'pending');
+    $result2 = getTodoItems($_SESSION['user_id'], 'done');
     include 'list.php';
-  } elseif ($valid_user === 'Email Exists'){
-    echo '<h2>Email Exists, Incorrect password</h2>';
-  } elseif ($valid_user === 'Email Does Not Exist'){
-    echo '<h2>Account Does Not Exist, ', '<a href="register.php">Register</a></h2>';
   }
-}
-
-else if ($action=='registrar') {
+  else if ($action == 'test_user') {
+    $email = filter_input(INPUT_POST, 'reg_email');
+    $password = filter_input(INPUT_POST, 'reg_password');
+    $valid_user = isUserValid($email, $password);
+    if ($valid_user === true) {
+      $result1 = getTodoItems($_SESSION['user_id'], 'pending');
+      $result2 = getTodoItems($_SESSION['user_id'], 'completed');
+      include 'list.php';
+    } elseif ($valid_user === 'Email Exists') {
+      echo '<h2>Email Exists, Incorrect password</h2>';
+    } elseif ($valid_user === 'Email Does Not Exist') {
+      echo '<h2>Account Does Not Exist, ', '<a href="register.php">Register</a></h2>';
+    }
+  }
+  else if ($action == 'registrar') {
     $first_name = filter_input(INPUT_POST, 'reg_first_name');
     $last_name = filter_input(INPUT_POST, 'reg_last_name');
     $phone = filter_input(INPUT_POST, 'reg_phone');
@@ -49,32 +38,38 @@ else if ($action=='registrar') {
 
     $user_exists = createUser($first_name, $last_name, $phone, $birthday, $gender, $email, $password);
 
-      if ($user_exists == true) {
-        include ('user_exists.php');
-      } else {
-        header("Location: index.php");
-       }
-   /*}*/
-}
-
-else if ($action == 'add') {
-  if (isset($_POST['title']) and $_POST['title'] != '') {
-    addTodoItem($_SESSION['user_id'], $_POST['title']);
+    if ($user_exists == true) {
+      include('user_exists.php');
+    } else {
+      header("Location: index.php");
+    }
+    /*}*/
   }
-  $result = getTodoItems($_SESSION['user_id']);
-  include ('list.php');
-}
-else if ($action == 'edit') {
-  /*Code to update the already set todo item's properties*/
-}
-
-else if($action == 'delete'){
-  if(isset($_POST['item_id'])) {
-    $selected = $_POST['item_id'];
+  else if ($action == 'add') {
+    if (isset($_POST['title']) and $_POST['title'] != '') {
+      addTodoItem($_SESSION['user_id'], $_POST['title']);
+    }
+    $result = getTodoItems($_SESSION['user_id']);
+    include('list.php');
+  }
+  else if ($action == 'edit') {
+    /*Code to update the already set todo item's properties*/
+  }
+  else if ($action == 'delete') {
+    $action = '';
+    $selected = filter_input(INPUT_POST, "todo_id");
     deleteTodoItem($_SESSION['user_id'], $selected);
+    $result1 = getTodoItems($_SESSION['user_id'], 'pending');
+    $result2 = getTodoItems($_SESSION['user_id'], 'completed');
+    include 'list.php';
   }
-  $result = getTodoItems($_SESSION['user_id']);
-  include ('list.php');
+
 }
 
+else {
+  $action="show_login_page";
+  if ($action == "show_login_page") {
+    include('login.php');
+  }
+}
 ?>
